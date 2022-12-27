954b69827e68e65207bfcb31b28cebbc8e403c0a
BeGeaMe <bekjonshavkatov0@gmail.com>
2022-12-27 13:38:38 +0500

    asasdsdsad

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,30 @@
 <template>
 <Home></Home>
 <Footer1></Footer1>
+<center></center>
 <Teacher></Teacher>
+<end></end>
+<darkend></darkend>
 </template>
 
 
 <script>
 
+import darkend from './components/darkend.vue'
+import end from './components/end.vue'
 import Home from'./components/Home.vue'
 import Footer1 from'./components/Footer.vue'
 import Teacher from'./components/Teachers.vue'
+import center from './components/center.vue'
+
 export default{
   components:{
   Home,
   Footer1,
-  Teacher
+  Teacher,
+  center,
+  end,
+  darkend,
 },
   data(){
     return{
