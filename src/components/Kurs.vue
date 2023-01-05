88b5a8645f07402a41ca2bfd2bfbb2e19eba1e4a
jaxongir2002 <jaxongirxon.rustamov12@gmail.com>
2023-01-05 19:08:09 +0300

    ozgardi

--- src/components/Kurs.vue
+++ src/components/Kurs.vue
@@ -1,149 +1,78 @@
 <template>
   <div class="container-fluid top" data-aos="zoom-out-up">
-    <h1
-      class="h1 w-50 py-2 pb-5"
-      style="
-        background: rgb(93, 191, 156);
-        background: linear-gradient(
-          13deg,
-          rgba(93, 191, 156, 1) 0%,
-          rgba(53, 73, 94, 1) 56%
-        );
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        border-bottom: 3px double rgb(93, 191, 156);
-      "
-    >
+    <h1 class="h1 w-50 py-2 pb-5 bag">
       Kurslar uchun texnalogiyalar
     </h1>
     <div class="container row w-100 mt-5 pt-5">
       <div class="col-12 col-md-4">
         <div>
-          <input
-            type="radio"
-            id="one"
-            value="One"
-            v-model="picked"
-            class="form-check-input fs-1"
-          />
+          <input type="radio" id="one" value="One" v-model="picked" class="form-check-input fs-1" />
           <label for="one" class="form-check-label fs-5">Frontend dasturlash</label>
         </div>
         <div>
-          <input
-            type="radio"
-            id="two"
-            value="two"
-            v-model="picked"
-            class="form-check-input fs-1"
-          />
+          <input type="radio" id="two" value="two" v-model="picked" class="form-check-input fs-1" />
           <label for="two" class="form-check-label fs-5">Backend dasturlash</label>
         </div>
         <div>
-          <input
-            type="radio"
-            id="three"
-            value="three"
-            v-model="picked"
-            class="form-check-input fs-1"
-          />
-          <label for="three" class="form-check-label fs-5">MERN</label>
+          <input type="radio" id="four" value="four" v-model="picked" class="form-check-input fs-1" />
+          <label for="four" class="form-check-label fs-5">Mobile dasturlash (Flutter)</label>
         </div>
         <div>
-          <input
-            type="radio"
-            id="four"
-            value="four"
-            v-model="picked"
-            class="form-check-input fs-1"
-          />
-          <label for="four" class="form-check-label fs-5"
-            >Mobile dasturlash (Flutter)</label
-          >
-        </div>
-        <div>
-          <input
-            type="radio"
-            id="five"
-            value="five"
-            v-model="picked"
-            class="form-check-input fs-1 radi"
-          />
-          <label for="five" class="form-check-label fs-5">Mobile dasturlash</label>
+          <input type="radio" id="five" value="five" v-model="picked" class="form-check-input fs-1 radi" />
+          <label for="five" class="form-check-label fs-5">Grafik dizayn</label>
         </div>
       </div>
 
       <div class="box col-md-8 col-12 pad">
         <div class="mt-5 d-flex justify-content-around">
-          <div class="card1 card box1" :class="{ rang: picked == 'One' }" >
+          <div class="card1 card box1" :class="{ rang: picked == 'One' }">
             <img src="../assets/html.png" alt="" class="d-block m-auto" />
           </div>
-          <div class="card1 card box1" :class="{ rang1: picked == 'One' }" >
+          <div class="card1 card box1" :class="{ rang1: picked == 'One' }">
             <img src="../assets/css-3.png" alt="" class="d-block m-auto" />
           </div>
-          <div class="card1 card box1" :class="{ rang2: picked == 'One' }" >
+          <div class="card1 card box1" :class="{ rang2: picked == 'One' }">
             <img src="../assets/bootstrap.png " alt="" class="d-block m-auto" />
           </div>
-          <div class="card1 card box1" :class="{ rang3: picked == 'One' }" >
+          <div class="card1 card box1" :class="{ rang3: picked == 'One' }">
             <img src="../assets/js.png" alt="" class="d-block m-auto" />
           </div>
-          <div class="card1 card box1" :class="{ rang4: picked == 'One' }" >
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
+          <div class="card1 card box1" :class="{ rang4: picked == 'One' }">
+            <img src="../assets/file_type_vue_icon_130078.png" alt="" class="d-block m-auto" />
           </div>
-          <div class="card1 card box1" :class="{ rang: back }" >
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
+          <div class="card1 card box1" :class="{ rang: picked == 'two' }">
+            <img src="../assets/python.png" alt="" class="d-block m-auto" />
           </div>
-          <div class="card1 card box1">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
-          </div>
-        </div>
-
-        <div class="mt-5 d-flex justify-content-around">
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
-          </div>
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
-          </div>
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
-          </div>
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
-          </div>
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
-          </div>
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
-          </div>
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
+          <div class="card1 card box1" :class="{ rang1: picked == 'two' }">
+            <img src="../assets/django.png" alt="" class="d-block m-auto" />
           </div>
         </div>
 
         <div class="mt-5 d-flex justify-content-around">
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
+          <div class="card1 card" :class="{ rang2: picked == 'two' }">
+            <img src="../assets/c-.png" alt="" class="d-block m-auto" />
           </div>
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
+          <div class="card1 card" :class="{ rang3: picked == 'two' }">
+            <img src="../assets/c-sharp.png" alt="" class="d-block m-auto" />
           </div>
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
+          <div class="card1 card" :class="{ rang: picked == 'four' }">
+            <img src="../assets/java.png" alt="" class="d-block m-auto" />
           </div>
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
+          <div class="card1 card" :class="{ rang2: picked == 'four' }">
+            <img src="../assets/flutter.jpg" alt="" class="d-block m-auto" />
           </div>
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
+          <div class="card1 card" :class="{ rang2: picked == 'five' }">
+            <img src="../assets/photoshop.png" alt="" class="d-block m-auto" />
           </div>
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
+          <div class="card1 card" :class="{ rang2: picked == 'five' }">
+            <img src="../assets/illustrator.png" alt="" class="d-block m-auto" />
           </div>
-          <div class="card1 card">
-            <img src="../assets/html.png" alt="" class="d-block m-auto" />
+          <div class="card1 card" :class="{ rang2: picked == 'five' }">
+            <img src="../assets/figma.png" alt="" class="d-block m-auto" />
           </div>
         </div>
+
+       
       </div>
     </div>
   </div>
@@ -155,8 +84,7 @@ export default {
   data() {
     return {
       picked: "One",
-      font: false,
-      back: false,
+   
     };
   },
 };
@@ -167,18 +95,22 @@ export default {
     width: 30px !important;
     height: 30px !important;
   }
+
   .card {
     width: 42px !important;
     height: 1% !important;
   }
+
   .srt {
     flex-wrap: wrap;
   }
+
   .box {
     bottom: 0 !important;
     left: 0 !important;
   }
 }
+
 .top {
   margin-top: 70px;
 }
@@ -223,7 +155,7 @@ img {
 .rang3 {
   transition-delay: 0.5s;
   transform: scale(1.2);
-  background-color: #8bc5ec7b; 
+  background-color: #8bc5ec7b;
   box-shadow: 4px 3px 15px #8bc5ec89;
 }
 
@@ -252,5 +184,15 @@ i {
   width: 10%;
   height: 80px !important;
   color: orange;
+}
+
+.bag {
+  background: rgb(93, 191, 156);
+  background: linear-gradient(13deg,
+      rgba(93, 191, 156, 1) 0%,
+      rgba(53, 73, 94, 1) 56%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  border-bottom: 3px double rgb(93, 191, 156);
 }
 </style>
--- src/components/center.vue
+++ src/components/center.vue
@@ -1,6 +1,5 @@
 
 <template>
-<<<<<<< HEAD
    <h1 class="h1 w-25 py-2 h1 "
       style="background: rgb(93,191,156);
        background: linear-gradient(13deg, rgba(93,191,156,1) 0%, rgba(53,73,94,1) 56%); -webkit-background-clip: text; -webkit-text-fill-color: transparent; border-bottom:3px double rgb(93,191,156)" >
@@ -10,15 +9,6 @@
          <v-sheet height="100%" class="box">
             <div class="d-flex fill-height justify-center align-center box" >
                <div class="text-h2">
-=======
-   <div>
-
-      <div class="centerdiv">
-         <div class="boxboss">
-            <div class="box1">
-               <div class="ramka">
-                  
->>>>>>> 08fc1ef18936aeb874f16196fe172f79604b8e13
                   <h1 class="text1 fw-bold salom1">Payment</h1>
                   <h1 class="text2 fw-bold salom2">Gets Easier</h1>
                   <p class="text-muted p w-75 fw-bold">{{ qiganish[i] }}</p>
@@ -72,138 +62,8 @@ export default {
    color: #5C0E62;
 }
 
-<<<<<<< HEAD
 .h1 {
    margin-top: 40px;
-=======
-
-.rasm {
-   border-radius: 80% 20% 70% 30% / 15% 66% 34% 85%;
-}
-
-.centerdiv {
-   border-radius: 0% 100% 0% 100% / 62% 0% 100% 38%;
-   background: white;
-   width: 100%;
-   height: 100vh;
-   transform: rotate(0.5turn);
-   position: relative;
-   bottom: 50px;
-   z-index: -1;
-}
-
-.boxboss {
-   width: 100%;
-   height: 70vh;
-   display: flex;
-   transform: rotate(0.5turn);
-   position: relative;
-}
-
-.box1 {
-   width: 50%;
-   height: 70vh;
-}
-
-.boxcha {
-   width: 50%;
-   height: 70vh;
-}
-
-@media (max-width: 800px) {
-   .ramka a {
-      position: relative;
-      top: 70px;
-   }
-
-   .shakl {
-      width: 30%;
-      height: 10vh;
-   }
-
-   .shakl h6 {
-      position: relative;
-      bottom: 500px !important;
-   }
-
-   .salom1 {
-      position: relative;
-      top: 40px;
-      width: 50%;
-
-   }
-
-   .salom2 {
-      width: 50%;
-   }
-}
-@media (max-width: 380px){
-   .boxcha{
-      position: relative;
-      bottom: 500px !important;
-      
-   }
-}
-
-
-@media (max-width: 550px) {
-   .boxboss {
-      display: block;
-   }
-
-   .box1 {
-      width: 100%;
-      height: 50vh;
-      position: relative;
-      top: 200px;
-   }
-
-   .boxcha {
-      width: 100%;
-      height: 50vh;
-      position: relative;
-      bottom: 600px;
-      background-image: url("../assets/phone2.png");
-      background-size: 80%;
-   }
-
-   .boxcha img {
-      display: none;
-   }
-
-   .shakl {
-      width: 20%;
-      height: 8vh;
-   }
-
-   .shakl h6 {
-      width: 300% !important;
-      position: absolute;
-      top: 30px;
-      left: 27px;
-      font-size: 10px;
-   }
-
-   .ramka a {
-      position: relative;
-      top: 50px;
-   }
-
-   .salom1 {
-      position: relative;
-      top: 30px;
-   }
-
-   .ramka {
-      width: 100%;
-      height: 50vh;
-   }
-
-   .ramka p {
-      width: 100%;
-
-   }
->>>>>>> 08fc1ef18936aeb874f16196fe172f79604b8e13
 }
 </style>
 
